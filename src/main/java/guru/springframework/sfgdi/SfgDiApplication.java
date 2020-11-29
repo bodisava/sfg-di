b038f23cea81d55e0332cf77ce98bdd3498e3f96
francisco <francisco.boato@gmail.com>
2020-11-29 14:55:32 +0100

    multiple properties files . lesson 89

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import guru.springframework.sfgdi.examplebeans.FakeJmsBroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +17,15 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 		
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
-		System.out.println("**** FakeDataSource *****");
+		System.out.println("----- FakeDataSource -----");
 		// Attenzione i valori del bean sono sovrascritti dalle variabile di ambiente
 		// lo username di maniera automatica e il password di maniera esplicita tramite il PropertyConfig
 		System.out.println(String.format("User: [%s]. password: [%s], url: [%s]", 
 				fakeDataSource.getUser(), fakeDataSource.getPassword(), fakeDataSource.getUrl()));
+		FakeJmsBroker jms = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println("----- JmsDataSource -----");
+		System.out.println(String.format("JMS-User: [%s]. JMS-password: [%s], JMS-url: [%s]", 
+				jms.getUser(), jms.getPassword(), jms.getUrl()));
 
 		PetController petController = ctx.getBean("petController", PetController.class);
 		System.out.println("--- The Best Pet is ---");
--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -5,13 +5,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import guru.springframework.sfgdi.examplebeans.FakeJmsBroker;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
+@PropertySources({
+	@PropertySource("classpath:datasource.properties"),
+	@PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
 	@Autowired
@@ -26,6 +32,15 @@ public class PropertyConfig {
 	@Value("${guru.dburl}")
 	String url;
 	
+	@Value("${guru.jms.username}")
+	String jmsUser;
+	
+	@Value("${guru.jms.password}")
+	String jmsPassword;
+	
+	@Value("${guru.jms.dburl}")
+	String jmsUrl;
+	
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
@@ -33,6 +48,15 @@ public class PropertyConfig {
 		fakeDataSource.setPassword(env.getProperty("PASSWORD"));
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
+	}
+	
+	@Bean 
+	public FakeJmsBroker fakeJmsBroker() {
+		FakeJmsBroker jms = new FakeJmsBroker();
+		jms.setUser(jmsUser);
+		jms.setPassword(jmsPassword);
+		jms.setUrl(jmsUrl);
+		return jms;
 	}
 	
 	@Bean
