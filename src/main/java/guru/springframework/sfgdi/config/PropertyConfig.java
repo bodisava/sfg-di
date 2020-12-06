38806c402bfc3d79bd2e321558efa8b5f0d1a495
francisco <francisco.boato@gmail.com>
2020-12-06 17:17:15 +0100

    using application.properties. lesson 90

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,28 +1,16 @@
 package guru.springframework.sfgdi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import guru.springframework.sfgdi.examplebeans.FakeJmsBroker;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
-@PropertySources({
-	@PropertySource("classpath:datasource.properties"),
-	@PropertySource("classpath:jms.properties")
-})
+
 public class PropertyConfig {
 
-	@Autowired
-	Environment env;
-	
 	@Value("${guru.username}")
 	String user;
 	
@@ -45,7 +33,7 @@ public class PropertyConfig {
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
 		fakeDataSource.setUser(user);
-		fakeDataSource.setPassword(env.getProperty("PASSWORD"));
+		fakeDataSource.setPassword(user);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
 	}
@@ -59,9 +47,5 @@ public class PropertyConfig {
 		return jms;
 	}
 	
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
+
 }
